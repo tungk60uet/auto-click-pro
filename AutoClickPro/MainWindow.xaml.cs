f7cb601aad32b466748349ea41d180a55f3328d6
Tung Teen <tungk60uet@gmail.com>
2018-11-16 18:10:32 +0700

    adding

--- AutoClickPro/MainWindow.xaml.cs
+++ AutoClickPro/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
-﻿using AutoClickPro.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,28 +22,49 @@ namespace AutoClickPro
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static string DBFolder = "database";
+        List<string> files=new List<string>();
         public MainWindow()
         {
             InitializeComponent();
+            getDB();
         }
-
+        void getDB()
+        {
+            lb_kichban.Items.Clear();
+            files.Clear();
+            if (!Directory.Exists(DBFolder))
+            {
+                Directory.CreateDirectory(DBFolder);
+            }
+            files.AddRange(Directory.GetFiles(DBFolder, "*.txt", SearchOption.TopDirectoryOnly));
+            foreach (string item in files)
+            {
+                lb_kichban.Items.Add(item.Substring(DBFolder.Length + 1));
+            }
+        }
         private void btn_add_kichban_Click(object sender, RoutedEventArgs e)
         {
-            if(txt_tenkichban.Text!="")
-                lb_kichban.Items.Add(txt_tenkichban.Text);
+            if (txt_tenkichban.Text != ""&&!File.Exists(DBFolder + "/" + txt_tenkichban.Text + ".txt"))
+            {
+                File.Create(DBFolder+"/"+txt_tenkichban.Text +".txt");
+                getDB();
+            }
         }
-
         private void btn_xoakichban_Click(object sender, RoutedEventArgs e)
         {
-            if(lb_kichban.SelectedIndex>-1)
-                lb_kichban.Items.RemoveAt(lb_kichban.SelectedIndex);
+            
+            if (lb_kichban.SelectedIndex > -1)
+            {
+                File.Delete(DBFolder + "/" + lb_kichban.SelectedValue);
+                getDB();
+            }
         }
-
         private void btn_chitietkichban_Click(object sender, RoutedEventArgs e)
         {
             if (lb_kichban.SelectedIndex > -1)
             {
-                chitietkichban chitietkichban = new chitietkichban(lb_kichban.SelectedIndex);
+                chitietkichban chitietkichban = new chitietkichban((string)lb_kichban.SelectedValue);
                 chitietkichban.Show();
             }
             
--- AutoClickPro/chitietkichban.xaml.cs
+++ AutoClickPro/chitietkichban.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace AutoClickPro
 {
@@ -20,10 +23,97 @@ namespace AutoClickPro
     /// </summary>
     public partial class chitietkichban : Window
     {
-        public chitietkichban(int idkichban)
+        int selectX;
+        int selectY;
+        int selectWidth;
+        int selectHeight;
+        public Pen selectPen;
+        bool start = false;
+        public chitietkichban(string tenkichban)
         {
             InitializeComponent();
-            Debug.WriteLine(idkichban);
+            this.Hide();
+            Take();
+            this.Show();
+
+        }
+        static BitmapImage LoadImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0) return null;
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(imageData))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
+        }
+        public static byte[] Take()
+        {
+            int screenWidth = Convert.ToInt32(SystemParameters.VirtualScreenWidth);
+            int screenHeight = Convert.ToInt32(SystemParameters.VirtualScreenHeight);
+            int screenLeft = Convert.ToInt32(SystemParameters.VirtualScreenLeft);
+            int screenTop = Convert.ToInt32(SystemParameters.VirtualScreenTop);
+
+            RenderTargetBitmap renderTarget = new RenderTargetBitmap(screenWidth, screenHeight, 96, 96, PixelFormats.Pbgra32);
+            VisualBrush sourceBrush = new VisualBrush();
+
+            DrawingVisual drawingVisual = new DrawingVisual();
+            DrawingContext drawingContext = drawingVisual.RenderOpen();
+
+            using (drawingContext)
+            {
+                drawingContext.PushTransform(new ScaleTransform(1, 1));
+                drawingContext.DrawRectangle(sourceBrush, null, new Rect(new Point(0, 0), new Point(screenWidth, screenHeight)));
+            }
+            renderTarget.Render(drawingVisual);
+
+            PngBitmapEncoder pngEncoder = new PngBitmapEncoder();
+            pngEncoder.Frames.Add(BitmapFrame.Create(renderTarget));
+
+            Byte[] _imageArray;
+
+            using (MemoryStream outputStream = new MemoryStream())
+            {
+                pngEncoder.Save(outputStream);
+                _imageArray = outputStream.ToArray();
+            }
+
+            using (FileStream stream = new FileStream(@"D:\test.png", FileMode.Create, FileAccess.ReadWrite))
+            {
+                using (BinaryWriter writer = new BinaryWriter(stream))
+                {
+                    writer.Write(_imageArray);
+                }
+            }
+
+            return _imageArray;
+        }
+
+        private void Grid_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void picturebox_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void picturebox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void picturebox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
         }
     }
 }
